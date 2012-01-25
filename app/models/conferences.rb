c4500e41a3bcbb4f44197a3e29bb8b1d528df8f9
Ben Klang <bklang@mojolingo.com>
2012-01-25 12:30:45 -0500

    Add field names for Conference rooms

--- app/models/conferences.rb
+++ app/models/conferences.rb
@@ -1,2 +1,8 @@
-class Conferences < ActiveRecord::Base
+class Conferences
+  include Mongoid::Document
+  # room_number and pin are strings so they can accomodate leading zeroes
+  field :room_number, type: String
+  field :name,        type: String
+  field :pin,         type: String
+  field :account,     type: String
 end
