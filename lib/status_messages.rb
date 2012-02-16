d18dbec4e55a37c277ab1fa83ff95de80a05c46e
Ben Klang <bklang@mojolingo.com>
2012-02-16 10:48:10 -0500

    Add #last_status_for method

--- lib/status_messages.rb
+++ lib/status_messages.rb
@@ -3,16 +3,24 @@ require 'httparty'
 class StatusMessages
   include HTTParty
   base_uri 'status.mojolingo.com/api'
-  
+
   def initialize(username, password)
     @auth = {:username => username, :password => password}
   end
-  
+
+  def default_options
+    {:basic_auth => @auth}
+  end
+
   def timeline(which = :public, since_id = 0, options={})
-    options = {
-      :basic_auth => @auth,
+    options = default_options.merge({
       :query => {:since_id   => since_id},
-    }.merge(options)
+    }).merge(options)
     self.class.get "/statuses/#{which}_timeline.json", options
+  end
+
+  def last_status_for(username, count = 1)
+    # the second parameter is unused, hence nil
+    timeline :user, nil, :query => { :screen_name => username, :count => count }
   end
 end
--- spec/lib/status_messages_spec.rb
+++ spec/lib/status_messages_spec.rb
@@ -1,36 +1,54 @@
 require 'spec_helper'
 
 describe StatusMessages do
-  before :each do
-    @s = StatusMessages.new 'user', 'pass'
+  describe '#timeline' do
+    before :each do
+      @s = StatusMessages.new 'user', 'pass'
+    end
+
+    it 'requests the correct URL' do
+      StatusMessages.expects(:get).once.with('/statuses/public_timeline.json', anything)
+      @s.timeline
+    end
+
+    it 'allows specifying the request type' do
+      StatusMessages.expects(:get).once.with('/statuses/friends_timeline.json', anything)
+      @s.timeline :friends
+    end
+
+    it 'allows specifying the start ID' do
+      StatusMessages.expects(:get).once.with(anything, has_value({:since_id => 999}))
+      @s.timeline :public, 999
+    end
+
+    it 'should get all status messages by default' do
+      args = {
+        :basic_auth => {
+          :username => 'user',
+          :password => 'pass',
+        },
+        :query => {
+          :since_id => 0,
+        },
+      }
+      StatusMessages.expects(:get).once.with(anything, has_entries(args))
+      @s.timeline
+    end
   end
-  
-  it 'requests the correct URL' do
-    StatusMessages.expects(:get).once.with('/statuses/public_timeline.json', anything)
-    @s.timeline
-  end
-  
-  it 'allows specifying the request type' do
-    StatusMessages.expects(:get).once.with('/statuses/friends_timeline.json', anything)
-    @s.timeline :friends
-  end
-  
-  it 'allows specifying the start ID' do
-    StatusMessages.expects(:get).once.with(anything, has_value({:since_id => 999}))
-    @s.timeline :public, 999
-  end
-  
-  it 'should get all status messages by default' do
-    args = {
-      :basic_auth => {
-        :username => 'user',
-        :password => 'pass',
-      },
-      :query => {
-        :since_id => 0,
-      },
-    }
-    StatusMessages.expects(:get).once.with(anything, has_entries(args))
-    @s.timeline
+
+  describe '#last_status_for' do
+    before :each do
+      @s = StatusMessages.new 'user', 'pass'
+    end
+
+    it 'should get latest single status for a specific user' do
+      StatusMessages.expects(:get).once.with('/statuses/user_timeline.json', has_value({:screen_name => 'otheruser', :count => 1}))
+      @s.last_status_for 'otheruser'
+    end
+
+    it 'should allow getting more than one status' do
+      StatusMessages.expects(:get).once.with('/statuses/user_timeline.json', has_value({:screen_name => 'otheruser', :count => 5}))
+      @s.last_status_for 'otheruser', 5
+    end
   end
 end
