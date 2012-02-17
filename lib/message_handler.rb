2a308f762ea07da7b0a74807ba7774b67d6a1f1a
Ben Klang <bklang@mojolingo.com>
2012-02-17 17:15:36 -0500

    Add handlers for "list projects", "help", invalid requests

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,26 +1,53 @@
 class MessageHandler
   class << self
     def respond_to(message)
-      case message.body
+      reply = message.reply
+      reply.body = case message.body
+      when /^list projects/
+        list_project_names message
       when /^get status/i
         get_status message
+      when /^help/i
+        help message
+      else
+        "Now you're not making any sense at all!"
       end
+      reply
+    end
+
+    def help(message)
+      <<-MSG
+      Things you can ask me:
+      get status [for username]
+      list projects [for customer]
+      help
+      MSG
     end
 
     def get_status(message)
       username = message.body.match(/^get status for (\w+)/)[1]
-      response = StatusMessages.last_status_for username
-      reply = message.reply
-      reply.body = case response.first
+      response = username.nil? ? StatusMessages.timeline(:public, 1) : StatusMessages.last_status_for(username)
+      case response.first
       when nil
         "No status updates found for #{username}"
       when Array
         # {"error"=>"No such user."} becomes ["error", "No such user."]
         response.has_key?('error') ? "Server reported an error: #{response['error']}" : "Unknown error processing this request."
       else
-        "@#{username}: \"#{response.first['text']}\""
+        status = response.first
+        "@#{status['user']['screen_name']}: \"#{status['text']}\""
       end
-      reply
+    end
+
+    def list_project_names(message)
+      client =  message.body.match(/^list projects for (\w+)/)[1] rescue nil
+      list = client.nil? ? Client.find(:all) : Client.where(name: client)
+      response = list.collect do |c|
+        c.projects.collect do |p|
+          "#{p.name} (#{c.name})"
+        end
+      end.flatten.join("\n")
+      response.empty? ? 'No matching projects found.' : response
     end
   end
 end
--- spec/lib/message_handler_spec.rb
+++ spec/lib/message_handler_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 class MockMessage
   attr_accessor :from, :to, :body
 
-  def initialize(from, to, body)
+  def initialize(from, to, body = nil)
     @from, @to, @body = from, to, body
   end
 
@@ -15,30 +15,68 @@ class MockMessage
 end
 
 describe MessageHandler do
-  it 'should request the latest status for a given user' do
-    input = MockMessage.new "bklang@mojolingo.com", "arabbit@mojolingo.com", "get status for blangfeld"
-
-    mock_status = [{'text' => 'Ben Was Here'}]
-    StatusMessages.expects(:last_status_for).once.with("blangfeld").returns mock_status
-
-    MessageHandler.respond_to(input).body.should == "@blangfeld: \"Ben Was Here\""
+  describe 'showing help message' do
+    it 'should respond with help text' do
+      @input = MockMessage.new 'bklang@mojolingo.com', 'arabbit@mojolingo.com', 'help'
+      MessageHandler.respond_to(@input).body.include?('Things you can ask me:').should be true
+    end
   end
 
-  it 'should respond with a generic message if no status messages are available' do
-    input = MockMessage.new "bklang@mojolingo.com", "arabbit@mojolingo.com", 'get status for blangfeld'
-
-    mock_status = []
-    StatusMessages.expects(:last_status_for).once.with("blangfeld").returns mock_status
-
-    MessageHandler.respond_to(input).body.should == "No status updates found for blangfeld"
+  describe 'receiving an invalid request' do
+    it 'should show a reasonable error message' do
+      @input = MockMessage.new 'bklang@mojolingo.com', 'arabbit@mojolingo.com', 'j23ijlksdvjlqk23;lkja;wlkj'
+      MessageHandler.respond_to(@input).body.should == "Now you're not making any sense at all!"
+    end
   end
 
-  it 'should properly handle server error messages' do
-    input = MockMessage.new 'bklang@mojolingo.com', 'arabbit@mojolingo.com', 'get status for wdrexler'
-    mock_status = {"error"=>"No such user.",
-                   "request"=>"/api/statuses/user_timeline.json?screen_name=wdrexler&count=1"}
-    StatusMessages.expects(:last_status_for).once.with('wdrexler').returns mock_status
+  describe 'fetching Status messages' do
+    before :each do
+      @input = MockMessage.new 'bklang@mojolingo.com', 'arabbit@mojolingo.com', 'get status for blangfeld'
+    end
 
-    MessageHandler.respond_to(input).body.should == "Server reported an error: No such user."
+    it 'should request the latest status for a given user' do
+      mock_status = [{'text' => 'Ben Was Here', 'user' => {'screen_name' => 'blangfeld'}}]
+      StatusMessages.expects(:last_status_for).once.with('blangfeld').returns mock_status
+
+      MessageHandler.respond_to(@input).body.should == '@blangfeld: "Ben Was Here"'
+    end
+
+    it 'should respond with a generic message if no status messages are available' do
+      mock_status = []
+      StatusMessages.expects(:last_status_for).once.with("blangfeld").returns mock_status
+
+      MessageHandler.respond_to(@input).body.should == "No status updates found for blangfeld"
+    end
+
+    it 'should properly handle server error messages' do
+      @input.body = 'get status for wdrexler'
+      mock_status = {"error"=>"No such user.",
+                     "request"=>"/api/statuses/user_timeline.json?screen_name=wdrexler&count=1"}
+      StatusMessages.expects(:last_status_for).once.with('wdrexler').returns mock_status
+
+      MessageHandler.respond_to(@input).body.should == "Server reported an error: No such user."
+    end
+  end
+
+  describe 'listing projects' do
+    before :each do
+      @input = MockMessage.new 'bklang@mojolingo.com', 'arabbit@mojolingo.com', 'list projects'
+    end
+
+    it 'should return a suitable message if no projects are found' do
+      @input.body = 'list projects for Invalid Customer'
+      MessageHandler.respond_to(@input).body.should == 'No matching projects found.'
+    end
+
+    it 'should list all projects when no customer name is specified' do
+      pending "Need fixtures"
+      MessageHandler.respond_to(@input).body.should == "Test Project (Big Customer)\nOther Project (Little Customer)"
+    end
+
+    it 'should list all projects filtered for a specific customer by name' do
+      pending "Need fixtures"
+      @input.body = 'list projects for Big Customer'
+      MessageHandler.respond_to(@input).body.should == 'Test Project (Big Customer)'
+    end
   end
 end
