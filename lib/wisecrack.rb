7191c217fca82c41f82062e0930f3b3a627c9b4e
Ben Klang <bklang@mojolingo.com>
2012-03-05 23:08:15 -0500

    Add ability to record time and search for clients

--- lib/wisecrack.rb
+++ lib/wisecrack.rb
@@ -14,7 +14,25 @@ class Wisecrack
     end
 
     def get_client_by_id(id)
-      make_request 'clients/getClient', id
+      make_request 'clients.getClient', id
+    end
+
+    def get_client_by_name(name)
+      # searchClients takes two arrays as params:
+      # * list of search strings
+      # * list of search fields
+      res = make_request 'clients.searchClients',  [name], ['company']
+
+      # The return value is a hash of results.
+      # Each search string is the key to an array of records matching the string
+      res[name].first rescue nil
+    end
+
+    def record_time(options = {})
+      unless options.has_key?('client') && options.has_key?('description') && options.has_key?('hours')
+        raise ArgumentError, "Must supply client, hours and description"
+      end
+      make_request 'time.recordTime', options
     end
   end
 end
