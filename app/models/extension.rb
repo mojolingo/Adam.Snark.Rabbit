b8599831d4733c4d14e1f8277f6dc5ecbfee460d
Ben Langfeld <ben@langfeld.me>
2012-04-12 11:15:58 +0100

    Nicer formatting

--- app/models/extension.rb
+++ app/models/extension.rb
@@ -1,3 +1,5 @@
 class Extension < ActiveLdap::Base
-  ldap_mapping :dn_attribute => 'uid', :prefix => 'ou=Extensions', :classes => ['top', 'AsteriskUser', 'AsteriskVoiceMail']
+  ldap_mapping :dn_attribute  => 'uid',
+               :prefix        => 'ou=Extensions',
+               :classes       => ['top', 'AsteriskUser', 'AsteriskVoiceMail']
 end
