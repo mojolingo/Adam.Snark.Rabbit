59023162ec60dc944b11c9c8b1688730d07cfbd3
Ben Langfeld <ben@langfeld.me>
2012-08-18 17:41:56 +0100

    Add mongodb to dev VM

--- dev_environment/cookbooks/adam/metadata.rb
+++ dev_environment/cookbooks/adam/metadata.rb
@@ -5,6 +5,7 @@ description       "Installs Adam Snark Rabbit"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 recipe            "adam", "Installs ruby, checks out adam"
+recipe            "apt-update", "Updates apt repositories"
 
 %w{ ubuntu debian }.each do |os|
   supports os
--- dev_environment/roles/dev.rb
+++ dev_environment/roles/dev.rb
@@ -4,4 +4,7 @@ run_list "recipe[apt]",
   "recipe[motd-tail]",
   "recipe[git]",
   "recipe[rbenv]",
+  "recipe[mongodb::10gen_repo]",
+  "recipe[adam::apt-update]",
+  "recipe[mongodb::default]",
   "recipe[adam]"
