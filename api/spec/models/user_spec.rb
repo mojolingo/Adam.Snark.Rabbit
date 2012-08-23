a20bbcc4a9df5a6575e199655a58f51b17c85858
Ben Langfeld <ben@langfeld.me>
2012-08-23 15:42:49 +0100

    Move auth grant specs to the correct file

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe User do
   context "created from Github OAuth" do
-    subject { AuthGrant.find_or_create_for_oauth(GithubMock.data).user }
-
-    its(:name)              { should == 'Ben Langfeld' }
-    its(:email)             { should == 'ben@langfeld.me' }
-    its(:social_usernames)  { should == { github: 'benlangfeld' } }
-
     it "should allow finding by github uid" do
       AuthGrant.find_or_create_for_oauth GithubMock.data
       expect(User.find_by_github_user_id(210221).name).to eq('Ben Langfeld')
@@ -15,11 +9,6 @@ describe User do
   end
 
   context "created from Twitter OAuth" do
-    subject { AuthGrant.find_or_create_for_oauth(TwitterMock.data).user }
-
-    its(:name)              { should == 'Ben Langfeld' }
-    its(:social_usernames)  { should == { twitter: 'benlangfeld' } }
-
     it "should allow finding by twitter uid" do
       AuthGrant.find_or_create_for_oauth TwitterMock.data
       expect(User.find_by_twitter_user_id('4508241').name).to eq('Ben Langfeld')
