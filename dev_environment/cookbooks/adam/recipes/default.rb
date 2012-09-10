ef9355630c535a9502c8a36732d52804917bfc9f
Ben Langfeld <ben@langfeld.me>
2012-09-10 09:44:25 +0100

    [DEV] Setup rails AMQP account with correct permissions

--- dev_environment/cookbooks/adam/recipes/default.rb
+++ dev_environment/cookbooks/adam/recipes/default.rb
@@ -1,4 +1,11 @@
 rabbitmq_user "rails" do
   password "password"
   action :add
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+rabbitmq_user "rails" do
+  vhost "/"
+  permissions '".*" ".*" ".*"'
+  action :set_permissions
 end
