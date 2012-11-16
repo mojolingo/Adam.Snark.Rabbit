7b1c39fb407e06c8e1fedbd4e1296897d4c6c0ce
Ben Langfeld <ben@langfeld.me>
2012-11-16 14:47:20 -0200

    Use berkshelf to fetch cookbooks
    Also add back adam cookbook

---
+++ dev_environment/cookbooks/adam/metadata.rb
@@ -0,0 +1,18 @@
+maintainer        "Mojo Lingo LLC"
+maintainer_email  "adam@mojolingo.com"
+license           "Proprietary"
+description       "Installs Adam Snark Rabbit"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           "1.0.0"
+recipe            "adam", "Installs ruby, checks out adam"
+recipe            "apt-update", "Updates apt repositories"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
+
+supports "mac_os_x", ">= 10.6.0"
+
+%w{ git rbenv }.each do |cb|
+  depends cb
+end
---
+++ dev_environment/cookbooks/adam/recipes/default.rb
@@ -0,0 +1,51 @@
+rabbitmq_user "rails" do
+  password "password"
+  action :add
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+rabbitmq_user "rails" do
+  vhost "/"
+  permissions '".*" ".*" ".*"'
+  action :set_permissions
+end
+
+rabbitmq_user "fingers" do
+  password "password"
+  action :add
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+rabbitmq_user "fingers" do
+  vhost "/"
+  permissions '".*" ".*" ".*"'
+  action :set_permissions
+end
+
+rabbitmq_user "brain" do
+  password "password"
+  action :add
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+rabbitmq_user "brain" do
+  vhost "/"
+  permissions '".*" ".*" ".*"'
+  action :set_permissions
+end
+
+rabbitmq_vhost "/test" do
+  action :add
+end
+
+rabbitmq_user "guest" do
+  password "pass"
+  action :add
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+rabbitmq_user "guest" do
+  vhost "/test"
+  permissions '".*" ".*" ".*"'
+  action :set_permissions
+end
