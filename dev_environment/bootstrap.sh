2121936a64ae814d8a617036c8e0e564ee9f55a0
Ben Langfeld <ben@langfeld.me>
2013-01-23 01:12:01 +0000

    [DEPLOY] Handle failure early when bootstrapping a box

--- dev_environment/bootstrap.sh
+++ dev_environment/bootstrap.sh
@@ -11,14 +11,14 @@ curl -L http://www.opscode.com/chef/install.sh | bash
 mkdir /var/chef
 
 rm roles.tgz
-wget --auth-no-challenge --http-user="$username" --http-password="$password" http://ci.mojolingo.com/job/Adam.Snark.Rabbit/lastBuild/artifact/build/roles.tgz
+wget --auth-no-challenge --http-user="$username" --http-password="$password" http://ci.mojolingo.com/job/Adam.Snark.Rabbit/lastBuild/artifact/build/roles.tgz || { echo 'downloading roles failed' ; exit 1; }
 tar -xf roles.tgz --directory /var/chef
 
 rm cookbooks.tgz
-wget --auth-no-challenge --http-user="$username" --http-password="$password" http://ci.mojolingo.com/job/Adam.Snark.Rabbit/lastBuild/artifact/build/cookbooks.tgz
+wget --auth-no-challenge --http-user="$username" --http-password="$password" http://ci.mojolingo.com/job/Adam.Snark.Rabbit/lastBuild/artifact/build/cookbooks.tgz || { echo 'downloading cookbooks failed' ; exit 1; }
 tar -xf cookbooks.tgz --directory /var/chef
 
-chef-solo -l debug -j dna.json
+chef-solo -l debug -j dna.json || { echo 'running chef failed' ; exit 1; }
 
 echo ""
 echo "Success! Adam Snark Rabbit was installed!"
