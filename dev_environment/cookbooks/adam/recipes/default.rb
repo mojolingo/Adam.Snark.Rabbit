3014a7cd587dbc335b66a1e0cba38d07a0f27c73
Ben Langfeld <ben@langfeld.me>
2013-01-23 22:46:50 +0000

    [DEPLOY] Have reverse proxy proxy_pass correct domain
    http://adam was being used, and making rails pass an incorrect redirect URL to OAuth providers

--- dev_environment/cookbooks/adam/recipes/default.rb
+++ dev_environment/cookbooks/adam/recipes/default.rb
@@ -71,6 +71,7 @@ if node[:adam][:standalone_deployment]
     nginx_load_balancer do
       application_port 3000
       application_server_role 'app'
+      proxy_pass node['adam']['root_domain']
     end
 
     before_restart do
