522cf0f4de3ff0e4cbdd219ff017993a66f5cda7
Ben Langfeld <ben@langfeld.me>
2013-01-25 00:08:45 +0000

    [BRAIN] Make sure translator neuron is enabled

--- brain/app.rb
+++ brain/app.rb
@@ -7,10 +7,12 @@ Bundler.require
 require_relative 'lib/amqp_handler'
 
 require_relative 'lib/humanity_neuron'
+require_relative 'lib/translator_neuron'
 
 brain = Brain.new
 
 brain.add_neuron HumanityNeuron.new
+brain.add_neuron TranslatorNeuron.new
 
 EM.run do
   AMQP.connect "amqp://#{ENV['ADAM_BRAIN_AMQP_USERNAME']}:#{ENV['ADAM_BRAIN_AMQP_PASSWORD']}@#{ENV['ADAM_BRAIN_AMQP_HOST']}" do |connection|
