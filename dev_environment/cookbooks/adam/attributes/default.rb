2f6c451c3135fb69ede72e2f4cb65308f320acc0
Ben Langfeld <ben@langfeld.me>
2013-01-26 12:22:07 +0000

    [DEPLOY] production/development RAILS_ENV as appropriate

--- dev_environment/cookbooks/adam/attributes/default.rb
+++ dev_environment/cookbooks/adam/attributes/default.rb
@@ -1,3 +1,4 @@
+default[:adam][:environment] = "production"
 default[:adam][:ruby_version] = "1.9.3-p194"
 default[:adam][:standalone_deployment] = true
 default[:adam][:deployment_path] = "/srv/adam"
