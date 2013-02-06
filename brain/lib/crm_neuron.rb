a42177da38698c50a0bcca208ce9f119f7369dd5
Ben Langfeld <ben@langfeld.me>
2013-02-06 22:30:52 +0000

    [BRAIN] Use phone/email address from data rather than static in CRM neuron

--- brain/lib/crm_neuron.rb
+++ brain/lib/crm_neuron.rb
@@ -11,6 +11,6 @@ class CRMNeuron
     futuresimple_token = message.user["profile"]["futuresimple_token"]
     session = Pipejump::Session.new token: futuresimple_token
     contact = session.contacts.all.find { |contact| contact.name == name }
-    "#{contact.name}, #{contact.title} at #{contact.organisation_name}\nPhone: +1 (515) 555-8765\nEmail: jsmith@acmeinc.com"
+    "#{contact.name}, #{contact.title} at #{contact.organisation_name}\nPhone: #{contact.phone}\nEmail: #{contact.email}"
   end
 end
