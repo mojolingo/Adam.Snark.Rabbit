6a4b39f707ffe13f7a465770efa9a7252b8ea1a9
Ben Langfeld <ben@langfeld.me>
2013-02-20 10:07:58 +0000

    [BRAIN] Make sure shutdown occurs when AMQP connection goes down

--- brain/lib/app.rb
+++ brain/lib/app.rb
@@ -15,16 +15,52 @@ class App < Adhearsion::Plugin
   end
 
   run :brain do
-    Adhearsion::Process.important_threads << Thread.new do
-      catching_standard_errors { boot }
-    end
+    boot
   end
 
   def self.boot
+    return unless Adhearsion::Process.state_name == :booting
+    m = Mutex.new
+    blocker = ConditionVariable.new
+
+    Adhearsion::Events.amqp_connected do
+      Adhearsion::Process.booted
+      m.synchronize { blocker.broadcast }
+    end
+
+    Adhearsion::Events.shutdown do
+      logger.info "Shutting down while connecting. Breaking the connection block."
+      m.synchronize { blocker.broadcast }
+    end
+
+    Adhearsion::Process.important_threads << Thread.new do
+      catching_standard_errors { connect_to_server }
+    end
+
+    # Wait for the connection to establish
+    m.synchronize { blocker.wait m }
+  end
+
+  def self.connect_to_server
+    raise 'Hello There'
+    run
+  rescue => e
+    # We only care about disconnects if the process is up or booting
+    return unless [:booting, :running].include? Adhearsion::Process.state_name
+
+    Adhearsion::Process.reset unless Adhearsion::Process.state_name == :booting
+
+    logger.error e
+    logger.fatal "AMQP connection failed. Going down."
+    Adhearsion::Process.stop!
+  end
+
+  def self.run
     EM.run do
       AMQP.connect "amqp://#{ENV['ADAM_BRAIN_AMQP_USERNAME']}:#{ENV['ADAM_BRAIN_AMQP_PASSWORD']}@#{ENV['ADAM_BRAIN_AMQP_HOST']}" do |connection|
         AMQPHandler.new(@brain).listen
         logger.info "Connected and listening for messages"
+        Adhearsion::Events.trigger :amqp_connected
       end
     end
   end
