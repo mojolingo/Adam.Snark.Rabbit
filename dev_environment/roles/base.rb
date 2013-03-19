43960ca45538c90451e1bf92f144c8019aae00ba
Ben Langfeld <ben@langfeld.me>
2013-03-19 13:54:38 -0300

    [DEPLOY] Don't indiscriminately upgrade packages

--- dev_environment/roles/base.rb
+++ dev_environment/roles/base.rb
@@ -3,5 +3,4 @@ description "Common components for all instances"
 run_list "recipe[apt]",
   "recipe[chef-solo-search]",
   "recipe[motd-tail]",
-  "recipe[adam::apt-update]",
   "recipe[adam::user]"
