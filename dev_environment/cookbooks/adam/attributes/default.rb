8d90590b5cd5626da008a5bf1ed7701768417f02
Ben Langfeld <ben@langfeld.me>
2013-03-20 12:33:21 -0300

    [BRAIN] Report exceptions to errbit

--- dev_environment/cookbooks/adam/attributes/default.rb
+++ dev_environment/cookbooks/adam/attributes/default.rb
@@ -3,3 +3,5 @@ default[:adam][:standalone_deployment] = true
 default[:adam][:deployment_path] = "/srv/adam"
 default[:adam][:app_repo_url] = "git@github.com:mojolingo/Adam.Snark.Rabbit.git"
 default[:adam][:app_repo_ref] = "master"
+default[:adam][:reporter][:url] = "http://errbit.mojolingo.com"
+default[:adam][:reporter][:api_key] = ""
