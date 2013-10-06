c15c4b8bc1321d5997bb57f04721f66fdd66169d
Ben Klang <bklang@mojolingo.com>
2013-10-06 01:44:00 -0400

    [FEATURE] Remove HTML encodings from some Wit response strings

--- brain/lib/wit.rb
+++ brain/lib/wit.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 require 'faraday_middleware'
+require 'cgi'
 
 class Wit
   BASE_URL = 'https://api.wit.ai'
@@ -17,7 +18,15 @@ class Wit
       req.params['q'] = message
       req.headers['Authorization'] = "Bearer #{ENV['WIT_API_KEY']}"
     end
+    remove_encoding response.body
+  end
 
-    response.body
+  def self.remove_encoding(message)
+    message['msg_body'] = CGI.unescapeHTML message['msg_body']
+    message['outcome']['entities'].keys.each do |entity|
+      message['outcome']['entities'][entity]['body'] = CGI.unescapeHTML message['outcome']['entities'][entity]['body'] 
+      message['outcome']['entities'][entity]['value'] = CGI.unescapeHTML message['outcome']['entities'][entity]['value'] 
+    end
+    message
   end
 end
