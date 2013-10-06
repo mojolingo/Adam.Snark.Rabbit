d03988c35446a1c61d4c6429522087b2331bcd4b
Ben Klang <bklang@mojolingo.com>
2013-10-06 00:57:21 -0400

    Switch to Faraday, since it's already in use

--- brain/lib/wit.rb
+++ brain/lib/wit.rb
@@ -1,17 +1,23 @@
-require 'typhoeus'
-require 'json'
+require 'faraday'
+require 'faraday_middleware'
 
 class Wit
   BASE_URL = 'https://api.wit.ai'
 
   def self.query(message)
-    request = Typhoeus::Request.new(
-      "#{BASE_URL}/message",
-      method: :get,
-      params: { q: message },
-      headers: { 'Authorization' => "Bearer #{ENV['WIT_API_KEY']}" }
-    )
+    conn = Faraday.new url: BASE_URL do |c|
+      c.basic_auth 'internal', ENV['ADAM_INTERNAL_PASSWORD']
+      c.adapter :net_http
+      c.response :logger
+      c.use FaradayMiddleware::ParseJson, content_type: 'application/json'
+      c.response :raise_error
+    end
 
-    JSON.decode request.run.body
+    response = conn.get '/message' do |req|
+      req.params['q'] = message
+      req.headers['Authorization'] = "Bearer #{ENV['WIT_API_KEY']}"
+    end
+
+    response.body
   end
 end
