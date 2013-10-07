18fbbc77f555c7048a736116eb26766544cfeb32
Ben Langfeld <ben@langfeld.me>
2013-10-07 18:48:24 -0300

    [EARS] Wire into the brain

--- ears/config/adhearsion.rb
+++ ears/config/adhearsion.rb
@@ -2,19 +2,12 @@
 
 $stdout.sync = true # Do not buffer stdout
 
-Adhearsion::Events.draw do
-  after_initialized do
-    logger.info "Connecting to ATT speech API...."
-    ATTSpeech.supervise_as :att_speech, ENV['ATT_ASR_KEY'], ENV['ATT_ASR_SECRET'], 'SPEECH'
-  end
-end
-
 Adhearsion.router do
-  route 'default' do
-    answer
-    speak "Hi, this is Adam. How can I help you?"
+  openended do
+    route 'default' do
+      answer
+      speak "Hi, this is Adam. How can I help you?"
 
-    loop do
       logger.debug "Listening for input"
       recording = record(start_beep: true, final_timeout: 1.seconds, format: 'wav', direction: :send).complete_event.recording
 
@@ -25,11 +18,8 @@ Adhearsion.router do
 
       interpretation = listener.value
       logger.debug "Interpretation from AT&T: #{interpretation.inspect}"
-
       message = interpretation.recognition.n_best.result_text
-      say "I think you said #{message}"
+      App.publish_message message, call.id
     end
-
-    hangup
   end
 end
