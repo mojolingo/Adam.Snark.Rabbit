79102fa539fa2e737f42116ce5c2a2df3e8f7b13
Ben Langfeld <ben@langfeld.me>
2013-10-07 19:13:26 -0300

    [EARS] Reject any ASR interpretation with confidence lower than 0.4 or where no speech was detected

--- ears/lib/gather_input_controller.rb
+++ ears/lib/gather_input_controller.rb
@@ -1,7 +1,7 @@
 class GatherInputController < Adhearsion::CallController
   def run
     logger.debug "Listening for input"
-    recording = record(start_beep: true, final_timeout: 1.seconds, format: 'wav', direction: :send).complete_event.recording
+    recording = record(start_beep: true, initial_timeout: 4.seconds, final_timeout: 1.seconds, format: 'wav', direction: :send).complete_event.recording
 
     logger.debug "Input received. Checking interpretation from AT&T..."
     listener = Celluloid::Actor[:att_speech].future.speech_to_text File.read(recording.uri.sub('file://', ''))
@@ -10,7 +10,12 @@ class GatherInputController < Adhearsion::CallController
 
     interpretation = listener.value
     logger.debug "Interpretation from AT&T: #{interpretation.inspect}"
-    message = interpretation.recognition.n_best.result_text
-    App.publish_message message, call.id
+    if interpretation.recognition.status == 'OK' && interpretation.recognition.n_best.confidence >= 0.4
+      message = interpretation.recognition.n_best.result_text
+      App.publish_message message, call.id
+    else
+      say "Sorry, I didn't catch that."
+      pass self.class
+    end
   end
 end
