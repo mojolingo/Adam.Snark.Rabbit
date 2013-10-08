940fcb9318c9930a0bdf9e4e32e9f0262d49b41b
Ben Klang <bklang@mojolingo.com>
2013-10-08 00:43:56 -0400

    The test should agree with itself

--- brain/spec/lib/time_neuron_spec.rb
+++ brain/spec/lib/time_neuron_spec.rb
@@ -6,7 +6,7 @@ describe TimeNeuron do
 
   it "should read the current time" do 
     Timecop.freeze Time.local(2013, 10, 9, 8, 7, 6)
-    subject.response(nil, nil).should =~ /The time is\s+8:09 AM/
+    subject.response(nil, nil).should =~ /The time is\s+8:07 AM/
   end
 end
 
