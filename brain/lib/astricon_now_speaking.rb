91397e9674c5a6b0a354fb422cdaadca70d13482
Ben Klang <bklang@mojolingo.com>
2013-10-08 14:38:43 -0400

    Split into smaller methods

--- brain/lib/astricon_now_speaking.rb
+++ brain/lib/astricon_now_speaking.rb
@@ -8,20 +8,13 @@ class AstriconNowSpeaking
 
   def reply(message, interpretation)
     entities = interpretation['outcome']['entities']
-    datetime = if entities.has_key? 'datetime'
-      begin
-        if entities['datetime']['value'].is_a? Hash
-          DateTime.parse entities['datetime']['value']['from']
-        else
-          # One last attempt to parse the time
-          DateTime.parse entities['datetime']['value']
-        end
-      rescue TypeError, ArgumentError
-        return "Sorry, I don't understand what time you mean?"
-      end
-    else
-      DateTime.now
+
+    begin
+      datetime = get_datetime entities
+    rescue TypeError, ArgumentError
+      return "Sorry, I don't understand what time you mean?"
     end
+
     sessions = AstriconPresentations.find_by_time datetime
 
     if sessions.count > 0
@@ -32,6 +25,24 @@ class AstriconNowSpeaking
     else
       message = "I don't see any presentations."
     end
+
     message
   end
+
+private
+
+  def get_datetime(entities)
+    if entities.has_key? 'datetime'
+      if entities['datetime']['value'].is_a? Hash
+        DateTime.parse entities['datetime']['value']['from']
+      else
+        # One last attempt to parse the time
+        DateTime.parse entities['datetime']['value']
+      end
+    else
+      DateTime.now
+    end
+  end
+  
 end
+
