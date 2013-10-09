dba56c1738d233fcb960e73f141957dc6fd7fb70
Ben Klang <bklang@mojolingo.com>
2013-10-09 15:57:21 -0400

    More human-like feedback to cover audio processing time

--- ears/lib/gather_input_controller.rb
+++ ears/lib/gather_input_controller.rb
@@ -6,7 +6,7 @@ class GatherInputController < Adhearsion::CallController
     logger.debug "Input received. Checking interpretation from AT&T..."
     listener = Celluloid::Actor[:att_speech].future.speech_to_text File.read(recording.uri.sub('file://', ''))
 
-    play recording.uri.sub('file://', '')
+    say ["Just a second while I check on that", "One moment please", "Hmmmm, checking", "Yes, ok, let me check", "Ok, let me look", "Right, let me check my sources"].sample
 
     interpretation = listener.value
     logger.debug "Interpretation from AT&T: #{interpretation.inspect}"
