6cae7d78103153aa0220896913a9eec06753b7e0
Ben Langfeld <ben@langfeld.me>
2013-11-06 17:05:16 -0200

    [BRAIN] Fix specs to use separate memory URL

--- brain/spec/spec_helper.rb
+++ brain/spec/spec_helper.rb
@@ -19,5 +19,6 @@ RSpec.configure do |config|
   config.before { WebMock.disable_net_connect! }
 end
 
+ENV['ADAM_MEMORY_URL'] = 'http://local.adamrabbit.com:3000'
 ENV['ADAM_ROOT_DOMAIN'] = 'local.adamrabbit.com:3000'
 ENV['ADAM_INTERNAL_PASSWORD'] = 'foobar'
