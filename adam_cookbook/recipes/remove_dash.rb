ce7143039ccc4229b01adaeeec9a03889b2f398b
Ben Langfeld <ben@langfeld.me>
2013-11-06 10:41:51 -0200

    [DEPLOY] Reconfigure dash non-interactively

--- adam_cookbook/recipes/remove_dash.rb
+++ adam_cookbook/recipes/remove_dash.rb
@@ -1,2 +1,2 @@
 # See https://github.com/ddollar/foreman/issues/384
-execute 'echo "dash dash/sh boolean false" | debconf-set-selections && dpkg-reconfigure dash'
+execute 'echo "dash dash/sh boolean false" | debconf-set-selections && dpkg-reconfigure dash -f noninteractive'
